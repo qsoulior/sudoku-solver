cd2bbfcbe0547a3b571d99d70391fd22015928dc
Vladimir <74735618+qsoulior@users.noreply.github.com>
2023-10-11 13:09:17 +0300

    Implement odd-even, X, figured sudoku solve

--- main.go
+++ main.go
@@ -8,15 +8,34 @@ import (
 
 func HandleIndex(w http.ResponseWriter, r *http.Request) {
 	if r.Method == http.MethodPost {
-		grid := new(SudokuGrid)
+		var body struct {
+			Mode   int
+			Values Grid
+			Layout Grid
+		}
 		d := json.NewDecoder(r.Body)
-		d.Decode(grid)
+		err := d.Decode(&body)
+		if err != nil {
+			w.WriteHeader(400)
+			return
+		}
 
-		solved := solve(grid)
+		var solved bool
+		switch body.Mode {
+		case 1:
+			solved = solveClassic(&body.Values)
+		case 2:
+			solved = solveX(&body.Values)
+		case 3:
+			solved = solveOddEven(&body.Values, &body.Layout)
+		case 4:
+			solved = solveFigured(&body.Values, &body.Layout)
+		}
+
 		if solved {
 			w.Header().Set("Content-Type", "application/json")
 			e := json.NewEncoder(w)
-			e.Encode(*grid)
+			e.Encode(body.Values)
 			return
 		}
 		w.WriteHeader(400)
--- solver.go
+++ solver.go
@@ -1,44 +1,155 @@
 package main
 
-type SudokuGrid [9][9]int
+type Grid [9][9]int
 
-func isValid(grid *SudokuGrid, row int, col int, num int) bool {
+// base
+func isValid(values *Grid, row int, col int, num int) bool {
 	for i := 0; i < 9; i++ {
-		if grid[row][i] == num {
+		if values[row][i] == num {
 			return false
 		}
 	}
 
 	for i := 0; i < 9; i++ {
-		if grid[i][col] == num {
+		if values[i][col] == num {
 			return false
-		}
-	}
-
-	startRow, startCol := row-row%3, col-col%3
-	for i := 0; i < 3; i++ {
-		for j := 0; j < 3; j++ {
-			if grid[i+startRow][j+startCol] == num {
-				return false
-			}
 		}
 	}
 
 	return true
 }
 
-func solve(grid *SudokuGrid) bool {
+// classic
+func isValidClassic(values *Grid, row int, col int, num int) bool {
+	startRow, startCol := row-row%3, col-col%3
+	for i := 0; i < 3; i++ {
+		for j := 0; j < 3; j++ {
+			if values[i+startRow][j+startCol] == num {
+				return false
+			}
+		}
+	}
+
+	return isValid(values, row, col, num)
+}
+
+// X
+func isValidX(values *Grid, row int, col int, num int) bool {
+	if row == col {
+		for i := 0; i < 9; i++ {
+			if values[i][i] == num {
+				return false
+			}
+		}
+	}
+
+	if row == 8-col {
+		for i := 0; i < 9; i++ {
+			if values[i][8-i] == num {
+				return false
+			}
+		}
+	}
+
+	return isValidClassic(values, row, col, num)
+}
+
+// figured
+func isValidFigured(values *Grid, layout *Grid, row int, col int, num int) bool {
+	color := layout[row][col]
 	for i := 0; i < 9; i++ {
 		for j := 0; j < 9; j++ {
-			if grid[i][j] == 0 {
-				for num := 1; num <= 9; num++ {
-					if isValid(grid, i, j, num) {
-						grid[i][j] = num
+			if color == layout[i][j] && values[i][j] == num {
+				return false
+			}
+		}
+	}
 
-						if solve(grid) {
+	return isValid(values, row, col, num)
+}
+
+// classic
+func solveClassic(values *Grid) bool {
+	for i := 0; i < 9; i++ {
+		for j := 0; j < 9; j++ {
+			if values[i][j] == 0 {
+				for num := 1; num <= 9; num++ {
+					if isValidClassic(values, i, j, num) {
+						values[i][j] = num
+
+						if solveClassic(values) {
 							return true
 						} else {
-							grid[i][j] = 0
+							values[i][j] = 0
+						}
+					}
+				}
+				return false
+			}
+		}
+	}
+	return true
+}
+
+// X
+func solveX(values *Grid) bool {
+	for i := 0; i < 9; i++ {
+		for j := 0; j < 9; j++ {
+			if values[i][j] == 0 {
+				for num := 1; num <= 9; num++ {
+					if isValidX(values, i, j, num) {
+						values[i][j] = num
+
+						if solveX(values) {
+							return true
+						} else {
+							values[i][j] = 0
+						}
+					}
+				}
+				return false
+			}
+		}
+	}
+	return true
+}
+
+// odd-even
+func solveOddEven(values *Grid, layout *Grid) bool {
+	for i := 0; i < 9; i++ {
+		for j := 0; j < 9; j++ {
+			if values[i][j] == 0 {
+				for num := layout[i][j]; num <= 9; num += 2 {
+					if isValidClassic(values, i, j, num) {
+						values[i][j] = num
+
+						if solveOddEven(values, layout) {
+							return true
+						} else {
+							values[i][j] = 0
+						}
+					}
+				}
+				return false
+			}
+		}
+	}
+	return true
+}
+
+// figured
+func solveFigured(values *Grid, layout *Grid) bool {
+	for i := 0; i < 9; i++ {
+		for j := 0; j < 9; j++ {
+			if values[i][j] == 0 {
+				for num := 1; num <= 9; num++ {
+					if isValidFigured(values, layout, i, j, num) {
+						values[i][j] = num
+
+						if solveFigured(values, layout) {
+							return true
+						} else {
+							values[i][j] = 0
 						}
 					}
 				}
